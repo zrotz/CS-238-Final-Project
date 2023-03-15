d65c5cec90c1dc41f9e7aae736f640fada743814
zrotz <126422883+zrotz@users.noreply.github.com>
2023-03-14 20:00:00 -0700

    First Viz of Q model
    Very ugly first viz attempt (just printing to terminal), but this is better than a heat map.

--- driver.py
+++ driver.py
@@ -17,7 +17,30 @@ def exploration(model : MDP, lr : float, meta : tuple, epoch : int):
             model.S.set_tup((bsp, psp, osp)) # update model state
         eg.reset()
         model.S.reset()
-    print(ql.Q)
+    return ql.Q
+
+def visualize_heatmap(model : MDP, Q, rl : int):
+    bus_location = 0
+    ped_y = np.random.randint(1, rl)
+    ped_x = np.random.randint(0, 3)
+
+    cur_state = model.State((bus_location, ped_y, 1 if ped_x == 1 else 0))
+
+    while cur_state._bs != (rl - 1): 
+        a = np.argmax(Q[cur_state.get_idx(rl)][:])
+        print(cur_state._bs, cur_state._ps, cur_state._os, a)
+
+        bs, ps, os = model.T.update(cur_state, a)
+        cur_state = model.State((bs, ps, os))
+
+
+    # end state
+    print(cur_state._bs, cur_state._ps, cur_state._os)
+
+
+
+
+
 
 def main():
     s0 = (0, 5, 0) # bus at start, ped. at row 5 on sidewalk
@@ -30,7 +53,9 @@ def main():
     lr = 0.2 # learning rate
     meta = (0.41, 0.9) # epsilon, decay rate
     epoch = 1e4
-    exploration(model, lr, meta, epoch)
+    Q = exploration(model, lr, meta, epoch)
+
+    visualize_heatmap(model, Q, 15)
 
 
 if __name__ == "__main__":
