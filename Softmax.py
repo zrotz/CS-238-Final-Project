b43e7728d418fc3c11a0f5450e6a3e545a8b7a50
zrotz <126422883+zrotz@users.noreply.github.com>
2023-03-16 13:13:01 -0700

    Softmax update

--- Softmax.py
+++ Softmax.py
@@ -6,13 +6,12 @@ class Softmax:
     def __init__(self, l : float, a : float):
         self._l = l  # precision paramter
         self._a = a  # precision factor
-        self._weights = [0.5, 0.5]  # initial probability for action 0,1
 
     def next_action(self, s : int, Q):
-        weights = [math.exp(self._l * i) for i in self._weights]
+        weights = [math.exp(self._l * Q[s][0]), math.exp(self._l * Q[s][1])]
         t = sum(weights)
-        self._weights = [i/t for i in weights]
-        return r.choice([0,1], p=self._weights)
+        norm_weights = [i/t for i in weights]
+        return r.choice([0,1], norm_weights)
     
     def decay(self):
         self._l *= self._a
